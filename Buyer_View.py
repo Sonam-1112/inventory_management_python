6e16877534db6fb0bc8d41ee617184c0e3d7bcdd
Sonam-1122 <sonamg2002@gmail.com>
2021-04-09 19:44:31 +0530

    Some changes

--- Buyer_View.py
+++ Buyer_View.py
@@ -9,54 +9,55 @@ import seaborn as sns
 import pymysql
 from datetime import datetime
 import import_ipynb
-import Manufacturer_View
-import Billing_Software
+# import Manufacturer_View
+# import Billing_Software
 
-def mains():
-    root = Tk()
-    root.geometry('1174x700+200+50')
-    root.resizable(False,False)
-    root.title('Buyer Dashboard')
-    root.configure(bg = 'gold2')
+# def mains():
+root = Tk()
+root.geometry('1174x700+200+50')
+root.resizable(False,False)
+root.title('Buyer Dashboard')
+root.configure(bg = 'gold2')
 
-    global con,mycursor
-    try:
-        con = pymysql.connect(host='localhost',user='root',password='')
-        mycursor = con.cursor()
-        query = 'use inventoryDB;'
-        mycursor.execute(query)
-    except:
-        messagebox.showerror('Notification','Some error occured in database!!!\nPlease try again...') 
+global con,mycursor
+try:
+    con = pymysql.connect(host='localhost',user='root',password='')
+    mycursor = con.cursor()
+    query = 'use inventoryDB;'
+    mycursor.execute(query)
+except:
+    messagebox.showerror('Notification','Some error occured in database!!!\nPlease try again...') 
+    
+def buyProduct():
+    pass
+    # Billing_Software.mains()
+#########################################################################Logout Button Function
+def logout():
+    rr = messagebox.askyesnocancel("Confirmation","Are you sure you want to exit?",parent=DataEntryFrame)
+    if(rr == True):
+        root.destroy() 
+##################################################ShowDataFrame##########################################
+ShowDataFrame = Frame(root,bg = 'gold2',relief=GROOVE,borderwidth=5,width=760,height=670)
+ShowDataFrame.place(x=410,y=20)
 
-    def buyProduct():
-        Billing_Software.mains()
-    #########################################################################Logout Button Function
-    def logout():
-        rr = messagebox.askyesnocancel("Confirmation","Are you sure you want to exit?",parent=DataEntryFrame)
-        if(rr == True):
-            root.destroy() 
-    ##################################################ShowDataFrame##########################################
-    ShowDataFrame = Frame(root,bg = 'gold2',relief=GROOVE,borderwidth=5,width=760,height=670)
-    ShowDataFrame.place(x=410,y=20)
+######################################################FrontPageButtonsFrame#################################
+DataEntryFrame = Frame(root,bg = 'gold2',relief=GROOVE,borderwidth=5,width=400,height=670)
+DataEntryFrame.place(x=10,y=20)
 
-    ######################################################FrontPageButtonsFrame#################################
-    DataEntryFrame = Frame(root,bg = 'gold2',relief=GROOVE,borderwidth=5,width=400,height=670)
-    DataEntryFrame.place(x=10,y=20)
+frontLabel = Label(DataEntryFrame,text='--------------Welcome-----------',width=21,font=('arial',22,'italic bold')
+                ,bg='gold2')
+frontLabel.place(x=0,y=0)
 
-    frontLabel = Label(DataEntryFrame,text='--------------Welcome-----------',width=21,font=('arial',22,'italic bold')
-                    ,bg='gold2')
-    frontLabel.place(x=0,y=0)
+homeBtn = Button(DataEntryFrame,text="1. Home",width=20,font=('arial',20,'italic'),bd=6,bg='red'
+            ,relief=RIDGE,activebackground='red',activeforeground='white')
+homeBtn.place(x=25,y=40)
 
-    homeBtn = Button(DataEntryFrame,text="1. Home",width=20,font=('arial',20,'italic'),bd=6,bg='red'
-                ,relief=RIDGE,activebackground='red',activeforeground='white')
-    homeBtn.place(x=25,y=40)
+productBtn = Button(DataEntryFrame,text="2. Buy Products",width=20,font=('arial',20,'italic'),bd=6,bg='light blue'
+            ,relief=RIDGE,activebackground='red',activeforeground='white',command=buyProduct)
+productBtn.place(x=25,y=120)
 
-    productBtn = Button(DataEntryFrame,text="2. Buy Products",width=20,font=('arial',20,'italic'),bd=6,bg='light blue'
-                ,relief=RIDGE,activebackground='red',activeforeground='white',command=buyProduct)
-    productBtn.place(x=25,y=120)
+logoutBtn = Button(DataEntryFrame,text="4. Logout",width=20,font=('arial',20,'italic'),bd=6,bg='light blue'
+            ,relief=RIDGE,activebackground='red',activeforeground='white',command=logout)
+logoutBtn.place(x=25,y=200)
 
-    logoutBtn = Button(DataEntryFrame,text="4. Logout",width=20,font=('arial',20,'italic'),bd=6,bg='light blue'
-                ,relief=RIDGE,activebackground='red',activeforeground='white',command=logout)
-    logoutBtn.place(x=25,y=200)
-
-    root.mainloop()
+root.mainloop()
